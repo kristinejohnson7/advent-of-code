5b611f0ee0c896c3f260330647a8260f925a17b2
Kristine Johnson <100108249+kristinejohnson7@users.noreply.github.com>
2023-12-01 18:11:47 -0500

    start test

--- solutions-2023/01/solution.ts
+++ solutions-2023/01/solution.ts
@@ -3,15 +3,17 @@ import { readFileSync } from 'fs'
 // https://adventofcode.com/2023/day/1
 // https://adventofcode.com/2023/day/1/input
 
-const testData = [
-  '1abc2',
-  'pqr3stu8vwx',
-  'a1b2c3d4e5f',
-  'treb7uchet',
-]
+const testData = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
 
 export const trebuchet = () => {
   const data = readFileSync(`${__dirname}/data.txt`, 'utf8').split('\n')
   console.log(testData)
-  return 0
+
+  let filteredArr = []
+  for (const item of data) {
+    const number = item.replace(/\D/g, '')
+    filteredArr.push(number)
+  }
+  console.log('filteredArr', filteredArr)
+  return filteredArr
 }
